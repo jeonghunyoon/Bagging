cf4d3a16fd3116af6332c53f6e9d67fbd05668b7
yoonjeonghun <jeonghun.yoon123@gmail.com>
2016-12-12 00:16:59 +0900

    [1212] data set load

--- Bagging.py
+++ Bagging.py
@@ -8,4 +8,28 @@ Output will be a average of results of base learners.
 '''
 
 import urllib
-import numpy as np
+
+### 1. Load data from UCI repository and
+xData = []
+yData = []
+f = urllib.urlopen("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv")
+
+# Split titles and datas
+lines = f.readlines()
+titles = lines[:1]
+lines = lines[1:]
+
+for line in lines:
+    tokens = line.strip().split(';')
+    # Extract target value
+    yData.append(float(tokens[-1]))
+    del(tokens[-1])
+    # Extract data
+    xData.append(map(float, tokens))
+
+nData = len(xData)
+nFeat = len(xData[0])
+
+### 2. Divide data set into train set and test set
+
+
