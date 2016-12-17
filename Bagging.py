03ed07a6cbb7646030a2214a291ec3bbb139aaba
yoonjeonghun <jeonghun.yoon123@gmail.com>
2016-12-18 02:13:00 +0900

    [1218] Bagging completed

--- Bagging.py
+++ Bagging.py
@@ -10,6 +10,9 @@ Output will be a average of results of base learners.
 import urllib
 import random
 from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from math import floor
+import matplotlib.pyplot as plot
 
 ### 1. Load data from UCI repository and
 xData = []
@@ -38,14 +41,16 @@ nTrain = len(xTrain)
 
 # Set parameters for ensemble model
 nBaseModel = 100
-treeDepth = 5
+treeDepth = 10
 modelList = []
 predList = []
 
 # Extract samples for bagging
-bagProp = 0.5
+bagProp = 1
 nBagSamples = int(len(xTrain) * bagProp)
 
+
+### 3. Fit models
 # Bootstrap Sampling (with replacement)
 for iBaseModel in range(nBaseModel):
     sampIdx = []
@@ -54,3 +59,42 @@ for iBaseModel in range(nBaseModel):
     sampIdx = sorted(sampIdx)
     xTrainBag = [xTrain[i] for i in sampIdx]
     yTrainBag = [yTrain[i] for i in sampIdx]
+
+    baseModel = DecisionTreeRegressor(max_depth=treeDepth)
+    # Fit a model (Previous models are independant to current fitting.)
+    baseModel.fit(xTrainBag, yTrainBag)
+    # Predict on test set
+    pred = baseModel.predict(xTest)
+    predList.append(pred)
+
+    modelList.append(baseModel)
+
+
+### 4. Assessment for these model.
+mse = []
+allPredictions = []
+nModel = len(modelList)
+for iModel in range(nModel):
+    prediction = []
+    # Prediction : average of models output
+    for iPred in range(len(xTest)):
+        pred = sum([predList[i][iPred] for i in range(iModel + 1)]) / (iModel + 1)
+        prediction.append(pred)
+    error = [(yTest[i] - prediction[i]) for i in range(len(yTest))]
+    mse.append(sum([e * e for e in error]) / len(error))
+    allPredictions.append(prediction)
+
+nModelIdx = [i+1 for i in range(nModel)]
+
+
+### 5. Plotting
+plot.figure()
+plot.plot(nModelIdx, mse)
+plot.axis("tight")
+plot.xlabel("Number of Models in Ensemble")
+plot.ylabel("Mean Squared Error")
+plot.ylim((0.0, max(mse)))
+plot.show()
+
+print ("Minimum MSE")
+print(min(mse))
